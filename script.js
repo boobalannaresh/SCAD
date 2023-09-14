d2ee6ca42a239d85c1276355176d635984b099b1
Naresh <103582823+boobalannaresh@users.noreply.github.com>
2023-09-14 15:55:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log(age);
+let age = 27
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var msg = "Superb";
+
+// function fun() {
+//     console.log(msg)
+//     var msg = "Superb";
+
+// }
+// fun()
+
